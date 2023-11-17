2891e6f9ebf4b3e270f3977b5fbff1a3828baf2a
shokod <delvinshoko@gmail.com>
2023-11-17 10:38:23 +0200

    Fix! Missing font type declarations

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Montserrat } from 'next/font/google';
+
+export const montserrat  = Montserrat ({ weight: ["400", "700"], subsets: ['latin'] });
+export const inter = Inter({ weight: "400", subsets: ['latin'] });
